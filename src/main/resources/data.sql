25ed90279f862c7d839d8151c7354b826e1fc5e8
Aleksey Kuznetsov <teaguest@yandex.ru>
2023-07-29 02:18:09 +0300

    feat: добавлена база данных, schema.sql, data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,23 @@
+INSERT INTO mpa (name)
+VALUES ('G- У фильма нет возрастных ограничений');
+INSERT INTO mpa (name)
+VALUES ('PG - Детям рекомендуется смотреть фильм с родителями');
+INSERT INTO mpa (name)
+VALUES ('PG-13 - Детям до 13 лет просмотр нежелателен');
+INSERT INTO mpa (name)
+VALUES ('R - Лицам до 17 лет просматривать фильм можно только в присутствии взрослого');
+INSERT INTO mpa (name)
+VALUES ('NC-17 - Лицам до 18 лет просмотр запрещён');
+
+INSERT INTO genres (name)
+VALUES ('Комедия');
+INSERT INTO genres (name)
+VALUES ('Драма');
+INSERT INTO genres (name)
+VALUES ('Мультфильм');
+INSERT INTO genres (name)
+VALUES ('Триллер');
+INSERT INTO genres (name)
+VALUES ('Документальный');
+INSERT INTO genres (name)
+VALUES ('Боевик');
